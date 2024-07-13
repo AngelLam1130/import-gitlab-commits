6b764697cffcf291a6f6b2019c136a0ced29a854
AngelLam1130 <anqi_lin1130@163.com>
2024-07-13 11:24:22 -0700

    project1

--- internal/app.go
+++ internal/app.go
@@ -93,7 +93,7 @@ func (a *App) Run(ctx context.Context) error {
 		}
 
 		for _, project := range projects {
-			if projectID == 0 {
+			if projectID == 1 {
 				commits, errCommit := a.doCommitsForProject(ctx, worktree, currentUser, project, lastCommitDate)
 				if errCommit != nil {
 					return fmt.Errorf("do commits: %w", errCommit)
@@ -101,6 +101,9 @@ func (a *App) Run(ctx context.Context) error {
 
 				projectCommitCounter[projectID] = commits
 				a.logger.Printf("project %d: commits %d", projectID, commits)
+			} else if projectID == 0 {
+				projectID = projectID + 1
+				continue
 			} else {
 				break
 			}
